d370939c3596a5020aece5ef3171680472d580c4
bmbsquared <60191699+bmbsquared@users.noreply.github.com>
2020-05-12 17:59:30 -0400

    Adding rotate function

---
+++ js/app.js
@@ -0,0 +1,25 @@
+let rotate = document.getElementsByTagName(`td`);
+rotate[0].addEventListener(`click` ,function(){
+    rotate[0].style.transform=`rotate(180deg)`;
+    rotate[1].style.transform=`rotate(0deg)`;
+    rotate[2].style.transform=`rotate(0deg)`;
+    rotate[3].style.transform=`rotate(0deg)`;
+});
+rotate[1].addEventListener(`click` ,function(){
+    rotate[1].style.transform=`rotate(180deg)`;
+    rotate[0].style.transform=`rotate(0deg)`;
+    rotate[2].style.transform=`rotate(0deg)`;
+    rotate[3].style.transform=`rotate(0deg)`;
+});
+rotate[2].addEventListener(`click` ,function(){
+    rotate[2].style.transform=`rotate(180deg)`;
+    rotate[0].style.transform=`rotate(0deg)`;
+    rotate[1].style.transform=`rotate(0deg)`;
+    rotate[3].style.transform=`rotate(0deg)`;
+});
+rotate[3].addEventListener(`click` ,function(){
+    rotate[3].style.transform=`rotate(180deg)`;
+    rotate[0].style.transform=`rotate(0deg)`;
+    rotate[2].style.transform=`rotate(0deg)`;
+    rotate[1].style.transform=`rotate(0deg)`;
+});
