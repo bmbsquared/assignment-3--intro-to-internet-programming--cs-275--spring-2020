1912d242896b36f39513fa43c0de0e271e8d51f4
bmbsquared <60191699+bmbsquared@users.noreply.github.com>
2020-05-12 21:07:28 -0400

    Messing with rotate function

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 let rotate = document.getElementsByTagName(`td`);
+
 rotate[0].addEventListener(`click` ,function(){
-    rotate[0].style.transform=`rotate(180deg)`;
+    rotate[0].style.transform =`rotate(180deg)`;
     rotate[1].style.transform=`rotate(0deg)`;
     rotate[2].style.transform=`rotate(0deg)`;
     rotate[3].style.transform=`rotate(0deg)`;
